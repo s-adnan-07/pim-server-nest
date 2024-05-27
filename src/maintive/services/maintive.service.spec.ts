c38e88d3070d85f954301dd76ae7e151d9ac8723
Adnan <s.adnan@aabtools.com>
2024-05-27 22:06:54 +0400

    Formatted spec files

--- src/maintive/services/maintive.service.spec.ts
+++ src/maintive/services/maintive.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { MaintiveService } from './maintive.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { MaintiveService } from './maintive.service'
 
 describe('MaintiveService', () => {
-  let service: MaintiveService;
+  let service: MaintiveService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MaintiveService],
-    }).compile();
+    }).compile()
 
-    service = module.get<MaintiveService>(MaintiveService);
-  });
+    service = module.get<MaintiveService>(MaintiveService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
