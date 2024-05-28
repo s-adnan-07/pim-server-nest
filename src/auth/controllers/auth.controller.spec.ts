a9b96ce52be56519cbbf06d35ef9d168b1f2ab81
Adnan <s.adnan@aabtools.com>
2024-05-28 23:08:34 +0400

    Added unit tests for login endpoint

--- src/auth/controllers/auth.controller.spec.ts
+++ src/auth/controllers/auth.controller.spec.ts
@@ -10,10 +10,17 @@ import { Person } from '@/users/schemas/person.schema'
 
 import { AuthController } from './auth.controller'
 import { AuthService } from '../services/auth.service'
+import { Response } from 'express'
+import { HttpStatus, UnauthorizedException } from '@nestjs/common'
+import LoginDetailsDto from '../dtos/login-details.dto'
 
 describe('AuthController', () => {
   let controller: AuthController
   let service: AuthService
+  let loginDetails: LoginDetailsDto
+  let res = {
+    cookie: jest.fn(),
+  } as unknown as Response
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,15 +30,51 @@ describe('AuthController', () => {
         UsersService,
         ConfigService,
         JwtService,
-        { provide: getModelToken(Person.name, 'pim-prod'), useValue: Model },
+        {
+          provide: getModelToken(Person.name, 'pim-prod'),
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
       ],
     }).compile()
 
     controller = module.get<AuthController>(AuthController)
     service = module.get<AuthService>(AuthService)
+
+    loginDetails = {
+      username: 'username',
+      password: 'password',
+    }
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  describe('login', () => {
+    it('should return username on success', async () => {
+      const token = 'token'
+      jest.spyOn(service, 'login').mockImplementation(async arg => token)
+
+      const result = await controller.login(loginDetails, res)
+
+      expect(res.cookie).toHaveBeenCalled()
+
+      expect(result).toStrictEqual({
+        statusCode: HttpStatus.OK,
+        message: 'success',
+        user: loginDetails.username,
+      })
+    })
+
+    it('should throw an unathorized error on fail', async () => {
+      jest.spyOn(service, 'login').mockImplementation(async () => null)
+
+      // don't use await here for rejects to work we need a promise
+      const result = controller.login(loginDetails, res)
+
+      await expect(result).rejects.toThrow(UnauthorizedException)
+    })
   })
 })
