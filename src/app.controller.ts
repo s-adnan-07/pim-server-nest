75bf166e4281ad79dd6b20e1fd83f312037b619b
Adnan <s.adnan@aabtools.com>
2024-05-29 11:40:25 +0400

    Updated home endpoint from private to public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { Public } from './shared/decorators/public.decorator'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Public()
+  getServerStatus(): string {
+    return this.appService.getServerStatus()
   }
 }
