91ce14f1b1ddae2cffdd902b25264b8559584b6b
Adnan <s.adnan@aabtools.com>
2024-05-29 11:39:35 +0400

    Setup mocks for product service test file

--- src/products/services/products.service.spec.ts
+++ src/products/services/products.service.spec.ts
@@ -1,12 +1,21 @@
+import { Model } from 'mongoose'
+
 import { Test, TestingModule } from '@nestjs/testing'
+import { getModelToken } from '@nestjs/mongoose'
+
 import { ProductsService } from './products.service'
+import { ItemStock } from '../schemas/item-stocks.schema'
 
 describe('ProductsService', () => {
   let service: ProductsService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        { provide: getModelToken('Product', 'pim-prod'), useValue: Model },
+        { provide: getModelToken(ItemStock.name, 'oms'), useValue: Model },
+      ],
     }).compile()
 
     service = module.get<ProductsService>(ProductsService)
