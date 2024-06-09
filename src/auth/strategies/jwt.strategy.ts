58ae8a06e531d271cc4470ac6a72cf0159fea1a3
Adnan <s.adnan@aabtools.com>
2024-06-09 13:26:58 +0400

    Replaced cookie auth with bearer token

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
-import { Strategy } from 'passport-jwt'
-import * as Extractor from 'passport-jwt-cookie-extractor'
+import { ExtractJwt, Strategy } from 'passport-jwt'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -11,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     // > Hence we don't mark the config service as private
 
     super({
-      jwtFromRequest: Extractor.fromCookie('jwt'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.get<string>('JWT_SECRET'),
       ignoreExpiration: false,
     })
